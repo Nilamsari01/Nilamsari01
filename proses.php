36c5b713863c13bdf55b6ab80d6e7adf2cb808c7
Nilam <nilamsari2446@gmail.com>
2026-04-07 10:10:29 +0700

    commit

---
+++ proses.php
@@ -0,0 +1,10 @@
+<?php
+$nama = $_POST['nama'];
+$email = $_POST['email'];
+$pesan = $_POST['pesan'];
+
+echo "Pesan berhasil dikirim!<br>";
+echo "Nama: $nama <br>";
+echo "Email: $email <br>";
+echo "Pesan: $pesan";
+?>
