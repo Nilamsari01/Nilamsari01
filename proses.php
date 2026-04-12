8a2d14c20874e474520182beef2de1afeadf2e4c
Nilam <nilamsari2446@gmail.com>
2026-04-12 19:37:24 +0700

    fix insyaallah

--- proses.php
+++ proses.php
@@ -1,10 +1,24 @@
 <?php
+include 'koneksi.php';
+echo "<pre>";
+var_dump($_POST);
+echo "</pre>";
+exit;
+
 $nama = $_POST['nama'];
 $email = $_POST['email'];
 $pesan = $_POST['pesan'];
 
-echo "Pesan berhasil dikirim!<br>";
-echo "Nama: $nama <br>";
-echo "Email: $email <br>";
-echo "Pesan: $pesan";
+// query insert
+$query = "INSERT INTO pesan (nama, email, pesan) 
+          VALUES ('$nama', '$email', '$pesan')";
+
+if (mysqli_query($conn, $query)) {
+    echo "Pesan berhasil dikirim!<br>";
+    echo "Nama: $nama <br>";
+    echo "Email: $email <br>";
+    echo "Pesan: $pesan";
+} else {
+    echo "Error: " . mysqli_error($conn);
+}
 ?>
