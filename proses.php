c3c6e799c6ce8883c120e5224816367086acb654
Nilam <nilamsari2446@gmail.com>
2026-04-12 19:55:50 +0700

    bismillah

--- proses.php
+++ proses.php
@@ -1,24 +1,19 @@
 <?php
 include 'koneksi.php';
-echo "<pre>";
-var_dump($_POST);
-echo "</pre>";
-exit;
 
 $nama = $_POST['nama'];
 $email = $_POST['email'];
 $pesan = $_POST['pesan'];
 
-// query insert
 $query = "INSERT INTO pesan (nama, email, pesan) 
           VALUES ('$nama', '$email', '$pesan')";
 
-if (mysqli_query($conn, $query)) {
-    echo "Pesan berhasil dikirim!<br>";
-    echo "Nama: $nama <br>";
-    echo "Email: $email <br>";
-    echo "Pesan: $pesan";
+$result = mysqli_query($conn, $query);
+
+if ($result) {
+    echo "BERHASIL MASUK DATABASE ✔<br>";
 } else {
-    echo "Error: " . mysqli_error($conn);
+    echo "GAGAL MASUK ❌<br>";
+    echo mysqli_error($conn);
 }
 ?>
